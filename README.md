1cbfe7817c63fbae885713c281a0d15fcf6ec639
Gonzalo Casas <gonzalo.casas@local.ch>
2016-07-03 16:48:32 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# ttn-microclient
-The Things Network microclient
+# The Things Network microclient
+
+This is a really tiny client for [The Things Network](https://thethingsnetwork.org) mainly built as an excuse to try out [now](https://zeit.co/now)'s mind-blowing service.
+
+It is completely self-contained: you can host it locally or deploy it to a provider and it will just work on its own with zero-configuration.
+
+
+## Prerequisites
+
+* [Node.JS](https://nodejs.org/)
+* An account and application in the [Dashboard](https://staging.thethingsnetwork.org/).
+* Install dependencies:
+
+    $ npm install
+
+
+## Getting Started
+
+Run the server:
+
+    $ npm start
+
+Browse to [localhost:8080](http://localhost:8080/)
+
+
+## Deploying
+
+Use [now](https://zeit.co/now)'s insanely awesome node.js service. Make sure you have installed `now` globally (`$ npm install -g now`).
+
+Login to now:
+
+    $ now --login 
+
+Deploy:
+
+    $ now
+
+Profit!
+
+
+## Credits
+
+Based on [Hylke Visser](https://github.com/htdvisser)'s [socket.io example](https://github.com/TheThingsNetwork/examples/tree/master/socketio) and [The Things Network SDK for Node.js](https://github.com/TheThingsNetwork/sdk/blob/master/apps/node/ttn/).
---
+++ server.js
@@ -0,0 +1,55 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var fs = require('fs');
+var ttn = require('ttn');
+
+// Run the webserver on port 8080
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Add Socket.io clients
+io.on('connection', function (socket) {
+
+  socket.on('start', function (data) {
+    var appEUI = data.appEUI;
+    console.log('Connecting to AppEUI: ' + appEUI);
+
+    // Start the TTN Client
+    var client = new ttn.Client('staging.thethingsnetwork.org', appEUI, data.accessKey);
+
+    // Forward connection ok
+    client.on('connect', function (msg) {
+      console.log('Connected to TTN MQTT');
+      socket.emit('ttn-connect', {})
+    });
+
+    // Forward uplink
+    client.on('uplink', function (msg) {
+      console.log('Uplink from Device: ' + msg.devEUI)
+      socket.emit('uplink', msg)
+    });
+
+    // Forward activations
+    client.on('activation', function (evt) {
+      console.log('Activated Device: ' + evt.devEUI)
+      socket.emit('activation', evt)
+    });
+
+    // Print errors to the console
+    client.on('error', function (err) {
+      console.log('Error: ' + err)
+    });
+
+    // Close the TTN client on exit
+    socket.on('disconnect', function () {
+      client.end()
+      console.log('Disconnected');
+    });
+  });
+
+});
